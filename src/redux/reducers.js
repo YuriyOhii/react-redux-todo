780c5a7a5f9de87d57555b65b56cc0c0e00af120
YuriyOhii <uaogiy@gmail.com>
2023-10-31 14:05:55 +0200

    all tasks are done

---
+++ src/redux/reducers.js
@@ -0,0 +1,55 @@
+const { combineReducers } = require("redux");
+
+const tasksInitState = [
+  { id: 0, text: 'Learn HTML and CSS', completed: true },
+  { id: 1, text: 'Get good at JavaScript', completed: true },
+  { id: 2, text: 'Master React', completed: false },
+  { id: 3, text: 'Discover Redux', completed: false },
+  { id: 4, text: 'Build amazing apps', completed: false },
+];
+
+const tasksReducer = (state = tasksInitState, action) => {
+  switch (action.type) {
+    case 'tasks/addTask': {
+      return [...state, action.payload];
+    }
+
+    case 'tasks/deleteTask': {
+      return [...state.filter(task => task.id !== action.payload)];
+    }
+    case 'tasks/toggleCompleted': {
+      return [
+        ...state.map(task => {
+          if (task.id === action.payload)
+            return { ...task, completed: !task.completed };
+          return task;
+        }),
+      ];
+    }
+
+    default:
+      return state;
+  }
+};
+
+const filtersInitState = {
+  status: 'all',
+};
+
+const filtersReducer = (state = filtersInitState, action) => {
+  switch (action.type) {
+    case 'filters/setFiltersStatus': {
+      return {
+        ...state,
+        status: action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+   tasks: tasksReducer,
+   filters: filtersReducer, 
+})
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,7 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
-const initState = {
-  tasks: [
-    { id: 0, text: 'Learn HTML and CSS', completed: true },
-    { id: 1, text: 'Get good at JavaScript', completed: true },
-    { id: 2, text: 'Master React', completed: false },
-    { id: 3, text: 'Discover Redux', completed: false },
-    { id: 4, text: 'Build amazing apps', completed: false },
-  ],
-  filters: {
-    status: 'all',
-  },
-};
+import { rootReducer } from './reducers';
 
 const enhancer = devToolsEnhancer();
 
-const rootReducer = (state = initState, actions) => state;
 export const store = createStore(rootReducer, enhancer);
